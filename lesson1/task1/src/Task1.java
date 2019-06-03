acc4e09ef2c7c78e20c86d7849e62d924d828508
Anton <ssa.ssa.ssa11@gmail.com>
2019-06-03 15:11:10 +0300

    Task 1 bitwise

--- lesson1/task1/src/Task1.java
+++ lesson1/task1/src/Task1.java
@@ -2,18 +2,16 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        int decimalNumber = 0, pos = 0;
-
+        int decimalNumber = 0;
 
         System.out.println("Enter a number in binary format: ");
 
         Scanner scanner = new Scanner(System.in);
-        int binaryNumber = scanner.nextInt();
+        String binaryNumber = scanner.nextLine();
 
-        int temp = binaryNumber;
-        while (temp != 0) {
-            decimalNumber += (temp % 10) * Math.pow(2, pos++);
-            temp /= 10;
+        for (char bit : binaryNumber.toCharArray()) {
+            decimalNumber <<= 1;
+            decimalNumber |= Character.getNumericValue(bit);
         }
 
         System.out.println(binaryNumber + " -> " + decimalNumber);
